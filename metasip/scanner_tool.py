1f599de6619c1321a0e1ce2e8bda52918fa715eb
Phil Thompson <phil@riverbankcomputing.com>
2012-05-14 09:55:30 +0100

    Added the basic GUI to the scanner tool.

--- metasip/scanner_tool.py
+++ metasip/scanner_tool.py
@@ -10,9 +10,7 @@
 # WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 
-from PyQt4.QtGui import QLabel
-
-from dip.model import implements, observe
+from dip.model import implements, List, Model, observe, Str, Trigger
 from dip.publish import ISubscriber
 from dip.shell import SimpleViewTool
 
@@ -47,11 +45,68 @@ class ScannerTool(SimpleViewTool):
     def view(self):
         """ Invoked to create the tool's view. """
 
+        from dip.ui import (ComboBox, FilesystemLocationEditor, Form, Grid,
+                GroupBox, Label, PushButton, Splitter, Stretch, VBox,
+                ViewStack)
+
+        # The view factory.
+        view_factory = ViewStack(
+                VBox(
+                    Label('no_project_text'),
+                    Stretch()),
+                Splitter(
+                    ViewStack(),
+                    VBox(
+                        Form(
+                            ComboBox('working_version', options='versions',
+                                    visible=False),
+                            FilesystemLocationEditor('source_directory')),
+                        GroupBox(
+                            VBox(
+                                Form('header_directory_suffix', 'file_filter',
+                                        'parser_arguments'),
+                                'update'),
+                                title="Properties"),
+                        Grid(
+                            PushButton('scan', enabled=False),
+                            PushButton('restart', label="Restart Workflow"),
+                            PushButton('new', label="New..."),
+                            PushButton('delete', enabled=False),
+                            nr_columns=2),
+                        Stretch())))
+
         # Create the view.
-        return QLabel("No header directories have been defined")
+        return view_factory(ScannerModel())
 
     @observe('subscription')
     def __subscription_changed(self, change):
         """ Invoked when the subscription changes. """
 
         print(change.new.model, "is", change.new.event)
+
+
+class ScannerModel(Model):
+
+    delete = Trigger()
+
+    no_project_text = Str("There is no project to scan.")
+
+    file_filter = Str()
+
+    header_directory_suffix = Str()
+
+    new = Trigger()
+
+    parser_arguments = Str()
+
+    restart = Trigger()
+
+    scan = Trigger()
+
+    source_directory = Str()
+
+    update = Trigger()
+
+    versions = List(Str())
+
+    working_version = Str(None, allow_none=True)
