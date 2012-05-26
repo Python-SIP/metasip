e2ae2701ba4b9eb95d825d303046fa5819782704
Phil Thompson <phil@riverbankcomputing.com>
2012-05-26 16:00:19 +0100

    Fixed the handling of unnamed structs.

--- metasip/GccXML.py
+++ metasip/GccXML.py
@@ -250,6 +250,9 @@ class _Struct(_ScopedItem, _Access):
         parser is the parser instance.
         scope is the scope to append the transformed entity to.
         """
+        if self.name is None:
+            return
+
         if self.incomplete:
             scope.content.append(
                     OpaqueClass(name=self.name, container=scope,
