e1b477fdfda50a3fb35cb5691bdc1e7bc8bb6db3
Phil Thompson <phil@riverbankcomputing.com>
2024-01-21 16:59:25 +0000

    Relicensed under BSD-2-Clause
    The project is now licensed under the BSD 2 clause license instead of the
    GPL v3.
    
    Resolves #4

--- metasip/project_io/__init__.py
+++ metasip/project_io/__init__.py
@@ -1,13 +1,6 @@
-# Copyright (c) 2023 Riverbank Computing Limited.
-#
-# This file is part of metasip.
-#
-# This file may be used under the terms of the GNU General Public License v3
-# as published by the Free Software Foundation which can be found in the file
-# LICENSE-GPL3.txt included in this package.
-#
-# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Copyright (c) 2024 Phil Thompson <phil@riverbankcomputing.com>
 
 
 from .abstract_project_ui import AbstractProjectUi
